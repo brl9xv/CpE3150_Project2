4b76636001b9e0b1d980f64d70a43ffca2f82483
Sanfan1 <617601745@qq.com>
2019-05-08 17:55:53 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,94 +1,99 @@
 
+void Middle_C();
+void Tenor_C();
+void Soprano_C();
+void SetDelay(long x);
+
 #include <asf.h>
-float offset=1;
+int offset=1;
 int MusicCycles=0;
 int DelayTime;
 int main (void)
 {
 	DDRA=0x00;  //set PA input
-	porta=0xFF;		//pull-up PA resistors
+	PORTA=0xFF;		//pull-up PA resistors
+	DDRD=0xFF;
 	DDRE=0x10;		//set speaker output
 	while(1){
-		if(PINA1)
-			Tune1();
-		if(PINA2)
-			Tune2();
-		if(PINA3)
-			Middle_C();
-		if(PINA4)
-			Tenor_C();
-		if(PINA5)
-			Soprano_C();
-		if (PINA6)
-			offset= offset-.10;
-		if(PINA7)
-			offset = offset+.10;
+		unsigned char temp = ~PINA;
+		if(temp & 0b00000001)
+		{
+		PORTD=0xFF;
+		Middle_C();
+		}
+		if(temp & 0b00000010)
+		Tenor_C();
+		if(PINA5==0)
+		Soprano_C();
+		if (temp & 0b00000100)
+		offset= offset;
+		if(temp & 0b00000100)
+		offset = offset;
 	}
 }
-void Middle_C(void){  //261.6256 Hz for .1 sec
-// delay = (261.6256Hz)^(-1) / 2 *16 M Hz =30578 MC
-//cycles for .1 second = .1/(261.6256 Hz)^-1 = 26 cycles
+void Middle_C(){  //261.6256 Hz for .1 sec
+	// delay = (261.6256Hz)^(-1) / 2 *16 M Hz =30578 MC
+	//cycles for .1 second = .1/(261.6256 Hz)^-1 = 26 cycles
 	SetDelay(30578*offset);
 	MusicCycles=(26/offset);
-	TIMSK=0x01
+	TIMSK0=0x01;
 	sei();
-	return;
 }
 
-void Tenor_C(void){  //523.2511 Hz for .1sec
-// delay = (523.2511Hz)^(-1) / 2 *16 M Hz =15289 MC
-//cycles for .1 second = .1/(523.2511 Hz)^-1 = 52 cycles
+void Tenor_C(){  //523.2511 Hz for .1sec
+	// delay = (523.2511Hz)^(-1) / 2 *16 M Hz =15289 MC
+	//cycles for .1 second = .1/(523.2511 Hz)^-1 = 52 cycles
 	SetDelay(15289*offset);
 	MusicCycles=(52/offset);
-	TIMSK=0x01
+	TIMSK0=0x01;
 	sei();
-	return;
 }
 
-void Soprano_C(void){  //1046.502 Hz for .1sec
-// delay = (1046.502Hz)^(-1) / 2 *16 M Hz =7645 MC
-//cycles for .1 second = .1/(1046.502 Hz)^-1 = 105 cycles
+void Soprano_C(){  //1046.502 Hz for .1sec
+	// delay = (1046.502Hz)^(-1) / 2 *16 M Hz =7645 MC
+	//cycles for .1 second = .1/(1046.502 Hz)^-1 = 105 cycles
 	SetDelay(7645*offset);
 	MusicCycles=(105/offset);
-	TIMSK=0x01
+	TIMSK0=0x01;
 	sei();
-	return;
 
 }
 
-void SetDelay(int x){
+void SetDelay(long x){
 	if(x>65280){
 		DelayTime=-(x/1024);
 		TCNT0=-(x/1024);
-		TCCR0=0x05;
+		TCCR0B=0x05;
 	}
 	else if(x>16320){
 		DelayTime=-(x/256);
 		TCNT0=-(x/256);
-		TCCR0=0x04;
+		TCCR0B=0x04;
 	}
 	else if(x>2040){
 		DelayTime=-(x/64);
 		TCNT0=-(x/64);
-		TCCR0=0x03;
+		TCCR0B=0x03;
 	}
 	else if(x>255){
 		DelayTime=-(x/8);
 		TCNT0=-(x/8);
-		TCCR0=0x02;
+		TCCR0B=0x02;
 	}
 	else if(x>0){
 		DelayTime=-x;
 		TCNT0=-(x);
-		TCCR0=0x01;
+		TCCR0B=0x01;
 	}
+	PORTD=0xF0;
 	return;
 }
-	
+
 ISR(TIMER0_OVF_vect){
+	PORTD=0x0F;
+	TCNT0=DelayTime;
 	if(MusicCycles>0){
 		MusicCycles-=1;
-		TCNT0=DelayTime;
 		PORTE^=0x10;
 	}
 }
